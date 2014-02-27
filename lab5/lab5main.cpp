7d2c6a1fb66ad57712a6a07a04c40f7f39f03760
Nicholas Abel <nick.a.abel@gmail.com>
2014-02-26 20:24:15 -0800

    Blank test and main

---
+++ lab5/lab5main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   lab5main.cpp
+ * Author: Nick
+ *
+ * Created on February 26, 2014, 8:20 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ lab5/lab5test.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   lab5test.cpp
+ * Author: Nick
+ *
+ * Created on February 26, 2014, 8:21 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
