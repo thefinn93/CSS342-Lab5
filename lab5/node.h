83155844f8c143e16241115966690f2e4f7d7de4
thefinn93 <thefinn93@thefinn93.com>
2014-02-28 11:52:09 -0800

    Added some overloads to node.h, touched up some docs

--- lab5/node.h
+++ lab5/node.h
@@ -8,49 +8,86 @@
  */
 class Node {
     public:
-    /**
-     * The empty constructor, initializes the node with empty data values
-     * and NULL child pointers
-     */
-    Node();
 
-    /**
-     * A full constructor, which accepts the full array of data that the node
-     * can hold, which are subsequently stored in the node.
-     * @param data          The NodeData to store in the node.
-     * @param left          A pointer to this node's left child (may be NULL)
-     * @param right         A pointer to this node's right child (may be NULL)
-     */
-    Node(NodeData data, char Node* left, Node* right);
+        /*-----------Comparison Operators-----------*/
+        /**
+         * Checks if the value of this Node's data is equal to tha of another
+         ** Node's
+         * @param right The node to compare to
+         * @return True if the two Node's data are equal, False otherwise.
+         */
+        Node& operator==(const Node& right) const;
 
-    /**
-     * Copy constructor. Copies the data over, leaving the pointers NULL.
-     * @param nodeToCopy
-     */
-    Node(const NodeData& nodeToCopy);
+        /**
+         * Checks if the value of this Node's data is less than that of another
+         ** Node's.
+         * @param right The node to compare to.
+         * @return  True if this Node's data is less than that of the right
+         ** Node's, False otherwise
+         */
+        Node& operator<(const Node& right) const;
 
-    /**
-     * @return The data stored in this node.
-     */
-    NodeData getData();
+        /**
+         * Checks if the value of this Node's data is greater than that of
+         ** another Node's.
+         * @param right The node to compare to.
+         * @return  True if this Node's data is greater than that of the right
+         ** Node's, False otherwise
+         */
+        Node& operator>(const Node& right) const;
 
-    /**
-     * Sets the data stored in this node.
-     * @param data  The data to store in this node
-     * @return True if successful.
-     */
-    bool setData(NodeData data);
+        /**
+         * Sets the data in this Node to the same values of the data of another
+         ** Node.
+         * @param right The node to copy the data from.
+         * @return This node.
+         */
+        Node& operator=(const Node& right);
 
-private:
+        /**
+         * The empty constructor, initializes the node with empty data values
+         * and NULL child pointers
+         */
+        Node();
 
-    /// The information to store in this node
-    NodeData data;
+        /**
+         * A full constructor, which accepts the full array of data that the node
+         * can hold, which are subsequently stored in the node.
+         * @param data          The NodeData to store in the node.
+         * @param left          A pointer to this node's left child (may be NULL)
+         * @param right         A pointer to this node's right child (may be NULL)
+         */
+        Node(NodeData data, Node* left, Node* right);
 
-    /// A pointer to this node's left child
-    NodeData* leftChildPtr;
+        /**
+         * Copy constructor. Copies the data over, leaving the pointers NULL.
+         * @param nodeToCopy
+         */
+        Node(const NodeData& nodeToCopy);
 
-    /// A pointer to this node's right child
-    NodeData* rightChildPtr;
+        /**
+         * Retreives the data element stored in this Node.
+         * @return The data stored in this node.
+         */
+        NodeData getData();
+
+        /**
+         * Sets the data stored in this node.
+         * @param data  The data to store in this node
+         * @return True if successful.
+         */
+        bool setData(NodeData data);
+
+    private:
+
+        /// The information to store in this node
+        NodeData data;
+
+        /// A pointer to this node's left child
+        NodeData* leftChildPtr;
+
+        /// A pointer to this node's right child
+        NodeData* rightChildPtr;
 
 };
 
