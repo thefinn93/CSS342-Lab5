beb81777440aa35c45506a70afa7e3e34ed9e434
thefinn93 <thefinn93@thefinn93.com>
2014-02-28 10:30:22 -0800

    Split NodeData and Node apart, made node.{cpp,h}

---
+++ lab5/node.h
@@ -0,0 +1,58 @@
+#ifndef NODE_H
+#define	NODE_H
+
+#include "nodeData.h"
+
+/**
+ * A node in a ThreadedBST
+ */
+class Node {
+    public:
+    /**
+     * The empty constructor, initializes the node with empty data values
+     * and NULL child pointers
+     */
+    Node();
+
+    /**
+     * A full constructor, which accepts the full array of data that the node
+     * can hold, which are subsequently stored in the node.
+     * @param data          The NodeData to store in the node.
+     * @param left          A pointer to this node's left child (may be NULL)
+     * @param right         A pointer to this node's right child (may be NULL)
+     */
+    Node(NodeData data, char Node* left, Node* right);
+
+    /**
+     * Copy constructor. Copies the data over, leaving the pointers NULL.
+     * @param nodeToCopy
+     */
+    Node(const NodeData& nodeToCopy);
+
+    /**
+     * @return The data stored in this node.
+     */
+    NodeData getData();
+
+    /**
+     * Sets the data stored in this node.
+     * @param data  The data to store in this node
+     * @return True if successful.
+     */
+    bool setData(NodeData data);
+
+private:
+
+    /// The information to store in this node
+    NodeData data;
+
+    /// A pointer to this node's left child
+    NodeData* leftChildPtr;
+
+    /// A pointer to this node's right child
+    NodeData* rightChildPtr;
+
+};
+
+#include "node.cpp"
+#endif /* NODEDATA_H */
