e0ec2b768c01e3dbda8fd26322eb41f9ae2c501d
thefinn93 <thefinn93@thefinn93.com>
2014-03-09 17:15:41 -0700

    Made a credits file

---
+++ lab5/credits.cpp
@@ -0,0 +1,14 @@
+#ifndef CREDITS
+#define CREDITS
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void credits() {
+    cout << "CSS342 Lab 5" << endl;
+    cout << "By Nick Abel and Finn Herzfeld" << endl;
+}
+
+#endif
