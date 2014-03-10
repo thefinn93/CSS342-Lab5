a7ac5a6c8b54c915ce7183dc6ef616ebc7fc0d23
thefinn93 <thefinn93@thefinn93.com>
2014-03-09 18:20:49 -0700

    Began writing tests for nodeData

--- lab5/test-nodeData.cpp
+++ lab5/test-nodeData.cpp
@@ -1,9 +1,30 @@
 #include "credits.cpp"
 #include "nodeData.h"
+#include <string>
 
 using namespace std;
 
+int WIDTH = 30;
+
+void testConstructors() {
+    cout.width(WIDTH);
+    cout << left << "Empty constructor:";
+    NodeData emptyConstructor = NodeData();
+    cout << "PASS (didn't crash)" << endl;
+
+    cout.width(WIDTH);
+    cout << left << "Full constructor:";
+    NodeData fullConstructor = NodeData((string)"Test", 1);
+    cout << "PASS (didn't crash)" << endl;
+
+    cout.width(WIDTH);
+    cout << left << "Token-only constuructor:";
+    NodeData tokenOnly = NodeData((string)"moretest");
+    cout << "PASS (didn't crash)" << endl;
+}
+
 int main(int argc, char** argv) {
     credits();
+    testConstructors();
     return 0;
 }
