0ff8593f15e9043537d31448328f2a42241c4607
thefinn93 <thefinn93@thefinn93.com>
2014-03-11 21:42:44 -0700

    Added operator tests

--- lab5/test-node.cpp
+++ lab5/test-node.cpp
@@ -46,6 +46,81 @@ void testConstructors() {
     delete data;
 }
 
+void testOperators() {
+    cout << "===================== Operators Test ===================" << endl;
+
+    cout.width(WIDTH);
+    cout << "Less than (<) operator:";
+    NodeData* apples = new NodeData("apples");
+    NodeData* bananas = new NodeData("bananas");
+    Node* left = new Node();
+    Node* right = new Node();
+    Node nodeApples = Node(apples, left, right, true, true);
+    Node nodeBananas = Node(bananas, left, right, true, true);
+
+    if(nodeBananas < nodeApples) {
+        cout << "PASS";
+    } else {
+        cout << "FAIL";
+    }
+    cout << endl;
+
+    cout.width(WIDTH);
+    cout << "Less than (<) operator negative:";
+    if(nodeApples < nodeBananas) {
+        cout << "FAIL";
+    } else {
+        cout << "PASS";
+    }
+    cout << endl;
+
+    cout.width(WIDTH);
+    cout << "Greater than (>) operator:";
+
+    if(nodeApples > nodeBananas) {
+        cout << "PASS";
+    } else {
+        cout << "FAIL";
+    }
+    cout << endl;
+
+    cout.width(WIDTH);
+    cout << "Greater than (>) operator negative:";
+    if(nodeBananas > nodeApples) {
+        cout << "FAIL";
+    } else {
+        cout << "PASS";
+    }
+    cout << endl;
+
+    Node nodeApples2 = Node(apples, left, right, true, true);
+    cout.width(WIDTH);
+    cout << "Equality (==) operator:";
+
+    if(nodeApples == nodeApples2) {
+        cout << "PASS";
+    } else {
+        cout << "FAIL";
+    }
+    cout << endl;
+
+    cout.width(WIDTH);
+    cout << "Equality (==) operator negative:";
+    if(nodeBananas == nodeApples) {
+        cout << "FAIL";
+    } else {
+        cout << "PASS";
+    }
+    cout << endl;
+
+    delete left;
+    delete right;
+    delete apples;
+    delete bananas;
+
+}
+
+
 int main() {
     credits();
     testConstructors();
