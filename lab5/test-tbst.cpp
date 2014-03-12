a1afb1c1cff94e83f149c3fdc79aed2c1202d989
thefinn93 <thefinn93@thefinn93.com>
2014-03-12 09:21:59 -0700

    Set the stage for some basic tests

--- lab5/test-tbst.cpp
+++ lab5/test-tbst.cpp
@@ -3,7 +3,31 @@
 
 int WIDTH = 30;
 
+void testConstructors() {
+    cout << "================== Testing Constructors =====================" << endl;
+    cout << "no tests yet defined" << endl;
+}
+
+void testInsertion() {
+    cout << "================== Testing Insertion =====================" << endl;
+    cout << "no tests yet defined" << endl;
+}
+
+void testTraversal() {
+    cout << "================== Testing Traversals =====================" << endl;
+    cout << "no tests yet defined" << endl;
+}
+
+void testRemove() {
+    cout << "================== Testing Removing =====================" << endl;
+    cout << "no tests yet defined" << endl;
+}
+
 int main() {
     credits();
+    testConstructors();
+    testInsertion();
+    testTraversal();
+    testRemove();
     return 0;
 }
