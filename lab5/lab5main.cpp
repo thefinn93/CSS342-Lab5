e7aaba80ee5fa8936b8da5db88156a090abfbb6b
Finn <finn@finn.io>
2014-03-13 22:52:15 -0700

    Added credits, removed extra couts

--- lab5/lab5main.cpp
+++ lab5/lab5main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include "tbst.h"
+#include "credits.cpp"
 
 using namespace std;
 
@@ -18,37 +19,31 @@ char const DEFAULT_FILE[] = "hamlet.txt";
  * 
  */
 int main(int argc, char** argv) {
-    
-    cout << "START";
-    
+    credits();
+
     ifstream infile(DEFAULT_FILE);
-    
+
     if (!infile) {
         cerr << "NO FILE OPEN" << endl;
+        exit(1);
     }
-    
-    
+
+
     ThreadedBST tokenTree;
-    
+
     char currentToken[99];
     char checkForSkipChar[1];
-    
-    
-    
-    cout << infile.eof();
-    
+
+
     while (!infile.eof()) {
-        cout << infile.peek();
         while (char(infile.peek()) == '\n' || char(infile.peek()) == '\t') {
             infile.ignore(1);
-            cout << infile.peek() << endl;
         }
         infile.getline(currentToken, 99, ' ');
         cout << currentToken << endl;
         tokenTree.insert(currentToken);
     }
-    
-    cout << tokenTree.getNumberOfNodes();
+
 
     return 0;
 }
