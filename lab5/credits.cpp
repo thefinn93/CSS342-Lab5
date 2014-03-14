09cc828495af503a70aacf498a860ee4fb625d18
Finn <finn@finn.io>
2014-03-13 22:26:09 -0700

    Credit updating

--- lab5/credits.cpp
+++ lab5/credits.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void credits() {
-    cout << "CSS342 Lab 5" << endl;
+    cout << "CSS342 Lab 5 - a Threaded Binary Search Tree" << endl;
     cout << "By Nick Abel and Finn Herzfeld" << endl;
 }
 
