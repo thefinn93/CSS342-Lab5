b9a65aacd29e4470a3bda61450ca86a1ca0d9b11
Nicholas Abel <nick.a.abel@gmail.com>
2014-03-13 18:42:40 -0700

    working on lab5main, getting it to read in a single file

--- lab5/lab5main.cpp
+++ lab5/lab5main.cpp
@@ -6,13 +6,28 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include "tbst.h"
 
 using namespace std;
+
+char const DEFAULT_FILE[] = "hamlet.txt";
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+    
+    ifstream& infile(DEFAULT_FILE);
+    
+    ThreadedBST tokenTree();
+    
+    char currentToken[];
+    
+    while (!infile.eof()) {
+        infile = infile.getline(currentToken, 99, ' ');
+        tokenTree.insert(currentToken);
+    }
 
     return 0;
 }
